6395097e065aecea79d0315cf54a722a50781cbd
David Goldstein <dg.goldstein@mail.utoronto.ca>
2018-05-10 20:32:22 -0400

    Pie chart for hardcoded directory working

--- fsChart.js
+++ fsChart.js
@@ -1,9 +1,19 @@
-var Chart = require('chart.js');
+const Chart = require('chart.js');
+const { findChildSizes } = require('./findSize');
 
+/**
+ * Generate an integer in the given range.
+ * 
+ * @param {Number} min The lower end of the range
+ * @param {Number} max The upper end of the range
+ */
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Generate a random RGB color.
+ */
 function getRandomColor() {
     let color = "rgb(" + 
         getRandomInt(0, 255) + "," +
@@ -13,30 +23,44 @@ function getRandomColor() {
     return color;
 }
 
-window.onload = function() {
-    var dataset = {
+/**
+ * Generates and formats the size data of a given directory so that
+ * it may be represented as a chart.
+ * 
+ * @param {String} dir The absolute path to a directory
+ */
+function generateSizeChartData(dir) {
+    let children = findChildSizes(dir).children.sort(function(a, b) {
+        return a.size - b.size;
+    });
+
+    let paths = [];
+    let sizes = [];
+    let backgroundColor = [];
+
+    children.forEach(e => {
+        paths.push(e.file);
+        sizes.push(e.size / 1024);
+        backgroundColor.push(getRandomColor());
+    });
+
+    return {
         datasets: [{
-            data: [39.10, 32.51, 13.68, 8.71, 6.01],
-            backgroundColor: [
-                getRandomColor(),
-                getRandomColor(),
-                getRandomColor(),
-                getRandomColor(),
-                getRandomColor()
-            ]
+            data: sizes,
+            backgroundColor: backgroundColor
         }],
-        labels: [
-            "IE", "Chrome", "Safari", "Firefox", "Others"
-        ]
+        labels: paths
     };
+}
+
+window.onload = function() {
+    var data = generateSizeChartData("./node_modules");
 
     var ctx = document.getElementById("donut").getContext("2d");
     var donut = new Chart(ctx, {
         type: "doughnut",
-        data: dataset,
-        options: {}
+        data: data,
+        options: {scaleLabel: "MB"}
     });
 
-    console.log(ctx);
-    console.log(donut);
 }
