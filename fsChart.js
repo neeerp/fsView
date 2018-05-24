386d1b1b6d69af64437ca8fcd6fbfeace673c051
David Goldstein <dg.goldstein@mail.utoronto.ca>
2018-05-23 20:22:52 -0400

    Refactored how chart data is generated and stored. Added file type to tooltip.

--- fsChart.js
+++ fsChart.js
@@ -3,85 +3,32 @@ const { fork } = require('child_process');
 const { getRandomInt, getRandomColor, formatBytes, formatTitle } = require('./utils');
 const ctx = document.getElementById("donut").getContext("2d");
 const path = require('path');
-let dir;
 let donut;
-let dirData;
-
-/**
- * Formats the child size data of a given directory so that
- * it may be represented as a chart.
- * 
- * @param {Object} data An object as returned by the findChildSizes method in findSize.js.
- * @return {Object} A data object for a chart.js chart.
- */
-function formatChartValues(data) {
-    // Declare the chart data object
-    let chartValues = {
-        datasets: [{
-            data: [],
-            backgroundColor: []
-        }],
-        labels: []
-    };
-
-    dirData = data;
-    dirData.children = dirData.children.sort(function(a, b) {
-        return b.size - a.size;
-    });
-
-    // Create an entry for the 15 largest children
-    let count = 0;
-    while (count < 16 && count < dirData.children.length) {
-        let cur = dirData.children[count];
-        chartValues.labels.push(path.basename(cur.name));
-        chartValues.datasets[0].data.push(cur.size);
-        chartValues.datasets[0].backgroundColor.push(getRandomColor());
-        count++;
-    }
-
-    // Aggregate the remaining files & folders into an 'other' section
-    if (count < dirData.children.length) {
-        let totalSize = 0;
-        while (count < dirData.children.length) {
-            let cur = dirData.children[count];
-            totalSize += cur.size;
-            count++;
-        }
-
-        chartValues.labels.push("Other");
-        chartValues.datasets[0].backgroundColor.push(getRandomColor());
-        chartValues.datasets[0].data.push(totalSize);
-    }
-
-    return chartValues;
-}
+let dirStack;
+let currentDir;
 
 /**
  * Generates a donut chart in the given canvas, and replaces the spinner.
  * 
  * @param {Object} data A data object for a Chart.js chart 
  */
-function generateChart(data) {
+function initializeChart() {
     donut = new Chart(ctx, {
         type: "doughnut",
-        data: formatChartValues(data),
         options: {
-            onClick: function (e, elements) {
-                if (elements.length) {
-                    let element = dirData.children[elements[0]._index];
-                    console.log(element);
-                    if (element.type === "directory") {
-                        stepIn(element);
-                    }
-                }
-            },
+            onClick: stepIn,
             tooltips: {
                 callbacks: {
-                    label: (i, d) => {
-                        let index = i.index;
+                    label: (t, d) => {
+                        let index = t.index;
                         let size = d.datasets[0].data[index];
                         let name = d.labels[index];
-                        return name + ": " + formatBytes(d.datasets[0].data[i.index]);
+                        return name + ": " + formatBytes(d.datasets[0].data[index]);
+                    },
+                    afterLabel: (t, d) => {
+                        let index = t.index;
+                        let type = d.datasets[0].fileType[index];
+                        return "Type: " + type;
                     }
                 }
             },
@@ -91,7 +38,6 @@ function generateChart(data) {
             title: {
                 display: true,
                 position: 'bottom',
-                text: formatTitle(data.name),
                 fontSize: 18,
                 fontStyle: "normal",
                 fontFamily: "'Roboto', sans-serif",
@@ -102,34 +48,72 @@ function generateChart(data) {
     });
 }
 
-function stepIn(element) {
-    element.parent = dirData;
-    donut.data = formatChartValues(element);
-    donut.options.title.text = formatTitle(element.name);
+/**
+ * Update the chart's data with a new file directory.
+ * 
+ * @param {Object} data A file object.
+ */
+function updateChart(data) {
+    currentDir = data;
+    donut.data = formatChartData(data);
+    donut.options.title.text = "Current Directory: " + data.name;
     donut.update();
 }
 
-function stepOut() {
-    console.log("hey");
-    if (dirData.parent) {
-        donut.data = formatChartValues(dirData.parent);
-        donut.options.title.text = formatTitle(dirData.name);
-        donut.update();
+/**
+ * Given a file object, formats it into data usable by the donut
+ * chart.
+ * 
+ * @param {Object} data A file object.
+ * @return {Object} A data object for a chart.js chart.
+ */
+function formatChartData(data) {
+    let children = data.children;
+    return {
+        labels: children.map(obj => {
+            return path.basename(obj.name);
+        }),
+        datasets: [{
+            data: children.map(obj => {
+                return obj.size;
+            }),
+            backgroundColor: children.map(obj => {
+                return getRandomColor();
+            }),
+            fileType: children.map(obj => {
+                return obj.type;
+            })
+        }]
     }
 }
 
+function stepIn(e, elements) {
+    if (elements.length) {
+        let nextDir = currentDir.children[elements[0]._index];
+        if (nextDir.type === "Directory") {
+            dirStack.push(currentDir);
+            updateChart(nextDir);
+        }
+    }
+}
 
+function stepOut() {
+    if (dirStack.length) {
+        let nextDir = dirStack.pop();
+        updateChart(nextDir);
+    }
+}
 
 /** Start creating the chart on document load */
 window.onload = function() {
     const worker = fork("findSize.js");
     worker.on('message', data => {
         console.log(data);
-        generateChart(data);
+        dirStack = [];
+        if (!donut) {initializeChart()};
+        updateChart(data);
         document.getElementById("loading").classList.add("hidden");
     });
     worker.send("C:\\Program Files");    
     document.getElementById("block").onclick = stepOut;
 }
-
-
