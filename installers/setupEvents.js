424ac34073f68fd6a24558e73c069c17352bd454
David Goldstein <dg.goldstein@mail.utoronto.ca>
2018-08-05 21:40:51 -0400

    Added packager/installer and explorer context menu opening

---
+++ installers/setupEvents.js
@@ -0,0 +1,41 @@
+const electron = require('electron')
+const app = electron.app
+const WinShell = require('../winshell.js');
+
+module.exports = {
+    handleSquirrelEvent: function() {
+        if (process.argv.length === 1) {
+            return false;
+        }
+
+        const squirrelEvent = process.argv[1];
+        switch (squirrelEvent) {
+            case '--squirrel-install':
+            case '--squirrel-updated':
+            // Optionally do things such as:
+            // - Add your .exe to the PATH
+            // - Write to the registry for things like file associations and
+            //   explorer context menus
+            WinShell.folderBackgroundContextMenu.register(app.quit);
+
+            return true;
+
+            case '--squirrel-uninstall':
+            // Undo anything you did in the --squirrel-install and
+            // --squirrel-updated handlers
+ 
+            WinShell.folderBackgroundContextMenu.deregister(app.quit())
+
+            return true;
+
+            case '--squirrel-obsolete':
+            // This is called on the outgoing version of your app before
+            // we update to the new version - it's the opposite of
+            // --squirrel-updated
+
+            app.quit();
+            return true;
+        }
+    }
+}
+
--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+//handle setupevents as quickly as possible
+const setupEvents = require('./installers/setupEvents')
+if (setupEvents.handleSquirrelEvent()) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return;
+}
+
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -6,9 +13,8 @@ const ipc = electron.ipcMain;
 const path = require('path');
 const url = require('url');
 
-let mainWindow;
-
 function main() {
+  let mainWindow;
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600, resizable: false});
 
@@ -19,12 +25,12 @@ function main() {
     slashes: true
   }));
 
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
   mainWindow.setMenu(null);
 
-  globalShortcut.register('f5', function() {
-		mainWindow.reload();
-  })
+  // globalShortcut.register('f5', function() {
+  //   mainWindow.reload();
+  // })
   
   // Send reply channel to the main window
   ipc.on('reply', (event, message) => {
@@ -32,9 +38,10 @@ function main() {
   });
   
 }
-
+ 
 app.on('ready', main);
 
 app.on('window-all-closed', () => {
   app.quit();
 });
+
